58b46328a69e2160fdcb65ae4768ffd5fc83f792
Sven Anders <sven@anders-hamburg.de>
2019-09-02 20:03:07 +0200

    Leerzeichen in Kommentar

--- src/app/t30-status-long-text.pipe.ts
+++ src/app/t30-status-long-text.pipe.ts
@@ -6,8 +6,8 @@ const LONG_TEXT = [
   'An allen angrenzenden Straßen mit Haupteingängen oder An- und Abreiseverkehr ist Tempo 30 eingerichtet.', /*2*/
   'In mindestens einer angrenzenden Straße mit Haupteingang oder An- und Abreiseverkehr fehlt Tempo-30.', /*3*/
   'Alle Tempo-30-Forderung(en) wurde von der Behörde abgelehnt.', /*4*/
-  //'Laut Behördenangabe ist hier Tempo 30 eingerichtet. Bitte prüfe vor Ort, ob wirklich an allen ' +
-  //'angrenzenden Straßen mit Haupteingängen oder An- und Abreiseverkehr Tempo 30 gilt.',
+  // 'Laut Behördenangabe ist hier Tempo 30 eingerichtet. Bitte prüfe vor Ort, ob wirklich an allen ' +
+  // 'angrenzenden Straßen mit Haupteingängen oder An- und Abreiseverkehr Tempo 30 gilt.',
   'Die Behörde hat bereits Tempo 30 angeordnet, nur das Schild steht noch nicht.', /* 5*/
 ];
 @Pipe({

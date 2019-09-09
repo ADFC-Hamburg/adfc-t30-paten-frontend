3c59d24e3c57274a2a367fcd09fb663fd712693a
Sven Anders <sven@anders-hamburg.de>
2019-09-09 12:13:55 +0200

    Version Mail

--- sendVersionMail.sh
+++ sendVersionMail.sh
@@ -9,15 +9,17 @@ Hallo,
 
 es gibt eine neue Version unter:
 
-https://tools.adfc-hamburg.de/t30-paten/version${new_version}
+https://tools.adfc-hamburg.de/t30-paten/${new_version}
 
 Änderungen:
 EOF
-git log --no-merges --pretty=medium version${old_version}..version${new_version} >>$output
+git log --no-merges --pretty=medium ${old_version}..${new_version} >>$output
 
 cat >>$output <<EOF
 
 --
 Diese Mail wurde automatisch erstellt von $0
 EOF
-mutt pg-karten@lists.hamburg.adfc.de -c tempo30@hamburg.adfc.de -c wiebke.hansen@hamburg.adfc.de -s "[ADFC-Paten] Neue Frontend Version ${new_version}" <$output
+
+cat $output
+mutt pg-karten@lists.hamburg.adfc.de -c tempo30@hamburg.adfc.de -c wiebke.hansen@hamburg.adfc.de -c julian.lindner@hamburg.adfc.de -s "[ADFC-Paten] Neue Frontend Version ${new_version}" <$output

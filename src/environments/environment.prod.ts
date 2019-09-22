71282eed63fcb704bb79b8554104b3a530fba86d
Sven Anders <sven@anders-hamburg.de>
2019-09-22 13:26:43 +0200

    Use Backend Version 0.1.13

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   VERSION: require('../../package.json').version,
-  API_BASE_URL: 'https://tools.adfc-hamburg.de/t30-paten/api/version0.1.12/api/',
+  API_BASE_URL: 'https://tools.adfc-hamburg.de/t30-paten/api/version0.1.13/api/',
   API_STUB_BASE_URL: 'https://tools.adfc-hamburg.de/t30-paten/backend/'
 };

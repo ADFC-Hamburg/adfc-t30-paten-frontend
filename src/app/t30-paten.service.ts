2fb9f73b82f792e8ac1e0822f0a89cf8149d41ef
Sven Anders <sven@anders-hamburg.de>
2019-09-22 13:23:17 +0200

    token submit to real api

--- src/app/t30-paten.service.ts
+++ src/app/t30-paten.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { T30Pate } from './t30pate';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { environment } from '../environments/environment';
@@ -18,33 +17,12 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class T30PatenService {
-  baseUrl = environment.API_STUB_BASE_URL;
-
+  baseUrl = environment.API_BASE_URL;
+  baseStubUrl = environment.API_STUB_BASE_URL;
   constructor(private http: HttpClient) {
   }
-  loadPatenschaft(id: number) {
-    return this.http.get<any>(this.baseUrl + 'pate-load.php?id=' + id, httpOptions)
-      .pipe(
-        map(res => {
-          console.log('res', res);
-          if (res.error) {
-            throw new NotificationError(res.error);
-          }
-          return res;
-        }));
-  }
-  savePatenschaft(pate: T30Pate) {
-    // FIXME statt any einen Typ angeben
-    return this.http.post<any>(this.baseUrl + 'pate-save.php', pate, httpOptions)
-      .pipe(
-        map(res => {
-          if (res.error) {
-            throw new NotificationError(res.error);
-          }
-        }));
-  }
   list() {
-    return this.http.get<any>(this.baseUrl + 'pate-list.php', httpOptions)
+    return this.http.get<any>(this.baseStubUrl + 'pate-list.php', httpOptions)
       .pipe(
         map(res => {
           console.log('res', res);
@@ -54,17 +32,9 @@ export class T30PatenService {
           return res;
         }));
   }
-  testToken(token: String) {
-    return this.http.get<any>(this.baseUrl + 'test-token.php?token=' + token, httpOptions)
-      .pipe(
-        map(res => {
-          if (res.error) {
-            throw new NotificationError(res.error);
-          }
-        }));
-  }
+
   submitToken(token: String) {
-    return this.http.get<any>(this.baseUrl + 'submit-token.php?token=' + token, httpOptions)
+    return this.http.get<any>(this.baseUrl + 'portal.php?verify=' + token, httpOptions)
       .pipe(
         map(res => {
           if (res.error) {

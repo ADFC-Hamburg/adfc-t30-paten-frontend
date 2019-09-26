51d672d941486afb7693647685ce10ba1fc2b5ca
Sven Anders <sven@anders-hamburg.de>
2019-09-26 11:56:51 +0200

    Kontaktdaten ins environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   VERSION: require('../../package.json').version,
   API_BASE_URL: 'https://tools.adfc-hamburg.de/t30-paten/api/version0.1.15/api/',
-  API_STUB_BASE_URL: 'https://tools.adfc-hamburg.de/t30-paten/backend/'
+  API_STUB_BASE_URL: 'https://tools.adfc-hamburg.de/t30-paten/backend/',
+  CONTACT_MAIL: 'tempo30sozial@hamburg.adfc.de',
+  CAMPAIN_URL: 'https://hamburg.adfc.de/tempo30sozial',
 };

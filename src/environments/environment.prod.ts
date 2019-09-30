169488bb5bdae1259d286fe7eb62538b1c51ab81
Sven Anders <sven@anders-hamburg.de>
2019-09-30 16:50:54 +0200

    Use backend 0.1.22

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   VERSION: require('../../package.json').version,
-  API_BASE_URL: 'https://tools.adfc-hamburg.de/t30-paten/api/version0.1.21/api/',
+  API_BASE_URL: 'https://tools.adfc-hamburg.de/t30-paten/api/version0.1.22/api/',
   CONTACT_MAIL: 'tempo30sozial@hamburg.adfc.de',
   CAMPAIN_URL: 'https://hamburg.adfc.de/tempo30sozial',
 };

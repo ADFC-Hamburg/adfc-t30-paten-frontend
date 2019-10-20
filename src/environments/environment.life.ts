f57a29d0c00ade9ec3d66a63494ec26e3476e1d3
Sven Anders <sven@anders-hamburg.de>
2019-10-20 15:05:52 +0200

    Deploy life

---
+++ src/environments/environment.life.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  VERSION: require('../../package.json').version,
+  API_BASE_URL: '/api/',
+  CONTACT_MAIL: 'tempo30sozial@hamburg.adfc.de',
+  CAMPAIN_URL: 'https://hamburg.adfc.de/tempo30sozial',
+};

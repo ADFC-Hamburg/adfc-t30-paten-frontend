12d07fbd07cc9af8e1825a48347f494e86be3cc9
Sven Anders <sven@anders-hamburg.de>
2019-10-27 06:35:37 +0100

    Positionsermitllung und Duplettenprüfung fuer neue Institutionen, fixes #197

--- src/app/services/t30-soziale-einrichtung.service.ts
+++ src/app/services/t30-soziale-einrichtung.service.ts
@@ -1,8 +1,11 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { SozialeEinrichtung } from '../sozialeEinrichtung';
 import { AbstractCrudService } from './abstractCrud.service';
+
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +20,7 @@ export class T30SozialeEinrichtungService extends AbstractCrudService<SozialeEin
   getEntityUrl() {
     return this.curdUrl + '?entity=institution';
   }
-
+  getNear(lat: number, lon: number): Observable<any> {
+    return this.http.get<any>(environment.API_BASE_URL + 'findNear.php?lat=' + lat + '&lon=' + lon);
+  }
 }

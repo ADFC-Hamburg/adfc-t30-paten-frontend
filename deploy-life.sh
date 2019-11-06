467c7a2aa23d29d849d0d1e532738e974ada8f7c
Sven Anders <sven@anders.hamburg>
2019-11-06 13:22:27 +0100

    deploy auch ohne globales ng tool

--- deploy-life.sh
+++ deploy-life.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ver=version$(grep version package.json |sed -e 's/\s*"version":\s"\(.*\)",/\1/')
-ng build --base-href=https://t30forderung.hamburg.adfc.de/ --configuration=life --deploy-url=https://t30forderung.hamburg.adfc.de/
+./node_modules/@angular/cli/bin/ng build --base-href=https://t30forderung.hamburg.adfc.de/ --configuration=life --deploy-url=https://t30forderung.hamburg.adfc.de/
     sed -i -e 's/\(data-base-url="\)."/\1\/"/' dist/t30-paten/index.html &&
-    rsync -r --delete -v dist/t30-paten/ root@tools:/var/www/t30forderung/frontend/
+    rsync -r --delete -v dist/t30-paten/ root@tools.adfc-hamburg.de:/var/www/t30forderung/frontend/
